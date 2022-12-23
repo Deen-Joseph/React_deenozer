38a5477cd763ba98e74882a3cb5700b7370e4095
joseph-deen <110327153+joseph-deen@users.noreply.github.com>
2022-12-23 17:27:31 +0530

    dummy

--- src/components/pages/Players.js
+++ src/components/pages/Players.js
@@ -12,7 +12,21 @@ const Players = () => {
       </div>
       
     </div>
+{/*================= Dummy Dummy=========== */}
+    <div class="card">
+<img src="img.jpg" alt="John" style="width:100%">
+<h1>John Doe</h1>
+<p class="title">CEO & Founder, Example</p>
+<p>Harvard University</p>
+<a href="#"><i class="fa fa-dribbble"></i></a>
+<a href="#"><i class="fa fa-twitter"></i></a>
+<a href="#"><i class="fa fa-linkedin"></i></a>
+<a href="#"><i class="fa fa-facebook"></i></a>
+<p><button>Contact</button></p>
+</div>
     </div>
+
+
   );
 };
 
